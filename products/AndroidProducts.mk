2956f2825a8f228c217cff9f1ddd67fde069f407
matt4321 <matthewandrewturnbull@gmail.com>
2014-12-18 20:31:49 +0000

    add flo

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -4,4 +4,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/rascarlo_hammerhead.mk \
     $(LOCAL_DIR)/rascarlo_mako.mk \
     $(LOCAL_DIR)/rascarlo_manta.mk \
-    $(LOCAL_DIR)/rascarlo_shamu.mk
+    $(LOCAL_DIR)/rascarlo_shamu.mk \
+    $(LOCAL_DIR)/matt4321_flo.mk
